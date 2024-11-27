8a1c3193618a2a016f6fd39c1c2166d9a84c0e7e
Leyris Théolise <5374935-theoliseLey@users.noreply.gitlab.com>
2024-11-27 11:45:41 +0100

    feat: add filters on type of areas

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,10 +2,27 @@
 import { ref } from 'vue'
 
 import { AREAS } from '@/constants/areas'
+import type { Area } from '@/types/areas'
 
 import MapOSM from '@/components/MapOSM.vue'
 
 const areasSelected = ref(AREAS)
+
+const isSelected = (filter: Area) => {
+  return areasSelected.value.some(
+    (selected) => selected.type === filter.type && selected.label === filter.label,
+  )
+}
+
+const toggleFilter = (filter: Area) => {
+  if (isSelected(filter)) {
+    areasSelected.value = areasSelected.value.filter(
+      (selected) => selected.type !== filter.type || selected.label !== filter.label,
+    )
+  } else {
+    areasSelected.value = [...areasSelected.value, filter]
+  }
+}
 </script>
 
 <template>
@@ -15,6 +32,24 @@ const areasSelected = ref(AREAS)
     <div class="dashboard">
       <div class="map">
         <MapOSM :areas="areasSelected" />
+
+        <div>
+          <div class="filters">
+            <span class="title">Types of areas</span>
+            <div v-for="(filter, index) in AREAS" :key="index" class="checkbox">
+              <label>
+                <input
+                  type="checkbox"
+                  :value="filter"
+                  :checked="isSelected(filter)"
+                  @change="toggleFilter(filter)"
+                />
+                <span class="checkmark"></span>
+                {{ filter.label }}
+              </label>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </main>
@@ -35,6 +70,44 @@ main {
     .map {
       display: flex;
       gap: 20px;
+
+      .filters {
+        display: flex;
+        flex-direction: column;
+        gap: 5px;
+
+        .title {
+          padding-bottom: 5px;
+        }
+
+        .checkbox {
+          display: flex;
+
+          label {
+            display: flex;
+            align-items: center;
+            gap: 5px;
+          }
+
+          input {
+            display: none;
+          }
+          .checkmark {
+            display: block;
+            border: 1px solid #e14eca;
+            width: 10px;
+            height: 10px;
+            border-radius: 1px;
+          }
+        }
+
+        .checkbox:hover input ~ .checkmark {
+          background-color: #ed9ee0;
+        }
+        .checkbox input:checked ~ .checkmark {
+          background-color: #e14eca;
+        }
+      }
     }
   }
 }
