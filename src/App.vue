6a4e8b2e1056105761c97285f8c41916664e482d
Leyris Théolise <5374935-theoliseLey@users.noreply.gitlab.com>
2024-11-27 10:30:44 +0100

    chore: init project

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import HomeView from '@/views/HomeView.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="leaf logo" src="`/leaf.svg`" width="20" height="20" />
+    <p>VégétationInTheCity</p>
+  </header>
+
+  <HomeView />
+</template>
+
+<style scoped>
+header {
+  padding: 0px 20px;
+  width: 100%;
+  border-bottom: 1px solid white;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h2>Espaces verts et arbres</h2>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+  padding: 0 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+</style>
